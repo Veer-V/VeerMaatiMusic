a461da22a198612ed18c2ffca66f25d0b4b7d158
Veer-V <princeofworld453@gmail.com>
2025-05-20 00:09:10 +0530

    meow

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 console.log('Lets write JavaScript');
 
 async function getSongs() {
-    let a = await fetch("http://192.168.0.107:3000/songs/");
+    let a = await fetch("http://192.168.0.104:3000/songs/");
     let response = await a.text();
 
     let div = document.createElement("div");
@@ -25,22 +25,26 @@ let audio = new Audio();
 const playMusic = (track) => {
     if (!track) return;
     audio.src = "songs/" + encodeURIComponent(track);
+    document.querySelector(".songinfo").innerHTML = decodeURIComponent(track);
+    document.querySelector(".songtime").innerHTML = "00:00 / 00:00";
     audio.play().catch(error => {
         console.error("Error playing audio:", error);
     });
 };
 
+let songs = [];
+
 async function main() {
     let currentSong;
     // Get the songs
-    let songs = await getSongs();
+    songs = await getSongs();
     console.log(songs);
     let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0];
     for (const song of songs) {
         songUL.innerHTML = songUL.innerHTML + `<li>
         <img class="invert" src="img/music.svg" alt="Music">
         <div class="info">
-            <div>${song.replaceAll("%20", " ")}</div>
+            <div>${decodeURIComponent(song)}</div>
             <div>Song Artist</div>
         </div><div class="playnow"><span>Play Now</span><img class="invert" src="img/Play.svg" alt="Play"></div></li>`;
     }
@@ -55,8 +59,42 @@ async function main() {
         }
     });
 
+     play.addEventListener("click", () => {
+    if (audio.paused) {
+        audio.play();                    // Start playing audio
+        play.src = "img/pause.svg";     // Change button icon to pause
+    } else {
+        audio.pause();                  // Pause the audio
+        play.src = "img/play.svg";      // Change button icon to play
+    }
+});
+
+
+// Previous Button
+previous.addEventListener("click", () => {
+    audio.pause();
+    console.log("Previous clicked");
+    const currentFilename = decodeURIComponent(new URL(audio.src).pathname.split("/").pop());
+    let index = songs.indexOf(currentFilename);
+    if (index > 0) {
+        playMusic(songs[index - 1]);
+    }
+});
+
+// Next Button
+next.addEventListener("click", () => {
+    audio.pause();
+    console.log("Next clicked");
+    const currentFilename = decodeURIComponent(new URL(audio.src).pathname.split("/").pop());
+    let index = songs.indexOf(currentFilename);
+    if (index < songs.length - 1) {
+        playMusic(songs[index + 1]);
+    }
+});
+
+
     // Play the first song (optional)
-    audio.src = "songs/" + encodeURIComponent(songs[0]);
+    playMusic(songs[0]);
     // audio.play();
 
     audio.addEventListener("loadeddata", () => {
